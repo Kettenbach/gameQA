5b0a1bbf77b6f611f4cd44818d7892a70d299ce9
ryansreel <ryansreel@gmail.com>
2025-05-30 10:05:25 -0500

    [ADD] Particle fx & export with browser setting instead of stand alone

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748553337|135666391';
+const CACHE_VERSION = '1748617414|56362166';
 /** @type {string} */
 const CACHE_PREFIX = 'twenty_eight-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
